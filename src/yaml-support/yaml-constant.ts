f2cfe33944aab4d387d15ded901869d8e0f59415
Kyle Finley <kyle@finley.sh>
2020-10-05 09:18:25 -0700

    add cfngin schema entry for yaml extension

--- src/yaml-support/yaml-constant.ts
+++ src/yaml-support/yaml-constant.ts
@@ -7,10 +7,15 @@ export const SNIPPETS_ROOT_PATH = join(__dirname, "../../snippets");
 
 export const YAML_SCHEMA_CONFIG_NAME_OF_VSCODE_YAML_EXTENSION = "yaml.schemas";
 
+export const CFNGIN_SCHEMA_FILENAME = "cfngin.schema.json";
 export const RUNWAY_SCHEMA_FILENAME = "runway.schema.json";
 
+export const CFNGIN_SCHEMA_FILE = Uri.file(
+  join(__dirname, `../../schemas/${CFNGIN_SCHEMA_FILENAME}`)
+).toString();
 export const RUNWAY_SCHEMA_FILE = Uri.file(
   join(__dirname, `../../schemas/${RUNWAY_SCHEMA_FILENAME}`)
 ).toString();
 
+export const CFNGIN_FILE_GLOBAL_PATTERN = ["**/*.cfn/*.yaml", "**/*.cfn/*.yml"];
 export const RUNWAY_FILE_GLOBAL_PATTERN = ["**/runway.yaml", "**/runway.yml"];
